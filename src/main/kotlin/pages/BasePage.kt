5451fb772c670c487923d0b6d93b6e6831df3438
Tauan de Araujo Fagundes <tauan_fagundes@terceiros.sicredi.com.br>
2023-04-21 09:00:48 -0300

    locator -> locatorString

--- src/main/kotlin/pages/BasePage.kt
+++ src/main/kotlin/pages/BasePage.kt
@@ -4,20 +4,20 @@ import utils.PlaywrightManager.page
 
 abstract class BasePage {
 
-    protected fun click(locator: String) {
-        page!!.locator(locator).click()
+    protected fun click(locatorString: String) {
+        page!!.locator(locatorString).click()
     }
 
-    protected fun type(text: String, locator: String) {
-        page!!.locator(locator).clear()
-        page!!.locator(locator).fill(text)
+    protected fun type(text: String, locatorString: String) {
+        page!!.locator(locatorString).clear()
+        page!!.locator(locatorString).fill(text)
     }
 
-    protected fun getText(locator: String): String = page!!.locator(locator).textContent().trim()
+    protected fun getText(locatorString: String): String = page!!.locator(locatorString).textContent().trim()
 
-    protected fun getFieldError(locator: String): String {
+    protected fun getFieldError(locatorString: String): String {
         val errorLocator = "//..//p"
-        val chainedLocator = "$locator >> $errorLocator"
+        val chainedLocator = "$locatorString >> $errorLocator"
         return getText(chainedLocator)
     }
 
