e44de83c0ba70c6b57450930fca14be615e9ffdf
Tauan de Araujo Fagundes <tauan_fagundes@terceiros.sicredi.com.br>
2023-05-11 11:58:49 -0300

    rollback to use submit in LoginPage

--- src/main/kotlin/pages/LoginPage.kt
+++ src/main/kotlin/pages/LoginPage.kt
@@ -1,6 +1,8 @@
 package pages
 
 import org.openqa.selenium.By
+import org.openqa.selenium.Keys
+import utils.DriverManager.driver
 
 private const val CARD = ".card__login"
 
@@ -8,7 +10,6 @@ class LoginPage : BasePage() {
 
     private val emailInput = By.cssSelector("$CARD input[name='email']")
     private val passwordInput = By.cssSelector("$CARD input[name='password']")
-    private val accessButton = By.cssSelector("$CARD .login__buttons > button[type='submit']")
     private val registerButton = By.cssSelector("$CARD .login__buttons > button[type='button']")
 
     fun clickRegister() = click(registerButton)
@@ -16,7 +17,7 @@ class LoginPage : BasePage() {
     fun doLogin(email: String, password: String) {
         type(email, emailInput)
         type(password, passwordInput)
-        click(accessButton)
+        driver!!.findElement(passwordInput).sendKeys(Keys.ENTER)
     }
 
 }
