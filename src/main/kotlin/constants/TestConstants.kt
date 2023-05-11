3268ed22c26202645d35923358212646ba790c4f
Tauan de Araujo Fagundes <tauan_fagundes@terceiros.sicredi.com.br>
2023-05-10 21:34:18 -0300

    removing properties ajusting end-to-end tests

--- src/main/kotlin/constants/TestConstants.kt
+++ src/main/kotlin/constants/TestConstants.kt
@@ -3,7 +3,7 @@ package constants
 const val ACCOUNT_PATTERN = "\\d+-\\d"
 const val LOGIN_EMAIL = "user1@test.com"
 const val LOGIN_NAME = "Test User 1"
+const val PASSWORD = "abc1234"
 const val TRANSFER_EMAIL = "user2@test.com"
 const val TRANSFER_NAME = "Test User 2"
-const val PASSWORD = "abc1234"
 const val TRANSFER_AMOUNT = "100.00"
