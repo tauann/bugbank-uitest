6fcab12bbaf92833cc7f5e69242ebdd401b6827b
Tauan de Araujo Fagundes <tauan_fagundes@terceiros.sicredi.com.br>
2023-05-11 11:51:47 -0300

    rollback to use submit in LoginPage

--- src/main/kotlin/pages/LoginPage.kt
+++ src/main/kotlin/pages/LoginPage.kt
@@ -1,12 +1,13 @@
 package pages
 
+import utils.PlaywrightManager.page
+
 private const val CARD = ".card__login"
 
 class LoginPage : BasePage() {
 
     private val emailInput = "$CARD input[name='email']"
     private val passwordInput = "$CARD input[name='password']"
-    private val accessButton = "$CARD .login__buttons > button[type='submit']"
     private val registerButton = "$CARD .login__buttons > button[type='button']"
 
     fun clickRegister() = click(registerButton)
@@ -14,7 +15,7 @@ class LoginPage : BasePage() {
     fun doLogin(email: String, password: String) {
         type(email, emailInput)
         type(password, passwordInput)
-        click(accessButton)
+        page!!.locator(passwordInput).press("Enter")
     }
 
 }
